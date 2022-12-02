e4587f02200fb209d8174a99cbd814d5620ec047
Luis <Luistejeda0011@gmail.com>
2022-12-02 19:48:31 -0400

    Adding comments section

---
+++ app/post/[id]/comments/page.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable @next/next/no-img-element */
+import axios from 'axios'
+import Link from 'next/link'
+
+export default async function Post ({ params }) {
+  const { id } = params
+  const host = 'https://ourspace-api.up.railway.app'
+
+  async function fetchComments () {
+    return await axios.get(`${host}/comments/find/${id}`, { next: { revalidate: 10 } })
+  }
+
+  const comments = await (await fetchComments()).data
+
+  return (
+    <div className=' border-solid border-r border-t border-l w-full border-gray-500'>{
+      comments.map((comment) => (
+        <div className='flex flex-col p-5 w-full mb-5 border-solid border-b border-gray-500 ' key={comment.Comment_ID}>
+          <Link href={`/user/${comment.userModel.user}`}>
+            <div className='flex gap-3'>
+              <div className='rounded-full w-12 h-12 bg-gray-50 grid place-items-center'>
+                <img className='rounded-full w-[88%] h-[88%]' src={comment.userModel.img} alt='user' />
+              </div>
+              <div className='h-12'>
+                <p className='font-medium'>{comment.userModel.First_Name + ' ' + comment.userModel.Last_Name}</p>
+                <h2 className='text-sm'>{comment.description}</h2>
+              </div>
+            </div>
+          </Link>
+        </div>
+      ))
+      }
+    </div>
+  )
+}
