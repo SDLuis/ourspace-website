9e1f1269c4fa2201c253b5ede1e11a24bf366cb1
Luis <Luistejeda0011@gmail.com>
2022-12-01 21:22:36 -0400

    Adding home link in navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,11 +1,12 @@
 'use client'
 
+import Link from 'next/link'
 import { Home, Message, Add } from './icons'
 
 export default function Navbar () {
   return (
-    <div className='px-4 w-full md:px-[13%] py-2 flex justify-between bg-[#000009] border-b border-solid border-gray-500 '>
-      <p className='text-4xl font-semibold text-white'>Ourspace</p>
+    <div className='px-4 w-full md:px-[13%] py-2 flex justify-between border-b border-solid border-gray-500 '>
+      <p className='text-4xl font-semibold text-white'><Link href='/home'>Ourspace</Link></p>
       <div className='flex gap-6 pt-2'>
         <Home />
         <Message />
