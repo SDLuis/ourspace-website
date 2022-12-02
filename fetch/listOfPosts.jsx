bc48355848d446acdbf0d13faadbc5dd25681de4
Luis <Luistejeda0011@gmail.com>
2022-12-02 19:50:18 -0400

    Adding link to single post and post comments

--- fetch/listOfPosts.jsx
+++ fetch/listOfPosts.jsx
@@ -16,19 +16,23 @@ export default async function Posts () {
   return (
     posts.map((post) => (
       <div className='flex flex-col p-5 w-[90%] sm:w-[440px] xl:w-[430px] border-solid border rounded-md mb-5 border-gray-500' key={post.Post_ID}>
-        <Link href={`/user/${post.userModel.user}`}>
-          <div className='flex gap-3'>
-            <div className='rounded-full w-16 h-16 bg-gray-50 grid place-items-center'>
-              <img className='rounded-full w-[88%] h-[88%]' src={`https://i.pravatar.cc/150?u=${post.userModel.user}`} alt='user' />
+        <div className='flex gap-3'>
+          <Link href={`/user/${post.userModel.user}`}>
+            <div className='rounded-full w-14 h-14 sm:w-16 sm:h-16 bg-gray-50 grid place-items-center'>
+              <img className='rounded-full w-[88%] h-[88%]' src={post.userModel.img} alt='user' />
             </div>
-            <div className='py-2 h-16'>
+          </Link>
+          <div className='py-1 sm:py-2 h-14 sm:h-16'>
+            <Link href={`/user/${post.userModel.user}`}>
               <p className='font-medium text-lg'>{post.userModel.First_Name + ' ' + post.userModel.Last_Name}</p>
               <p className='text-sm'>{post.Location}</p>
-            </div>
+            </Link>
           </div>
+        </div>
+        <Link href={`/post/${post.Post_ID}`}>
+          <h2 className='my-3'>{post.description}</h2>
+          <img width={400} height={500} className='object-cover rounded-md w-[525px] h-[300px]' src={post.img} alt='post image' />
         </Link>
-        <h2 className='my-3'>{post.description}</h2>
-        <img width={400} height={500} className='object-cover rounded-md w-[525px] h-[300px]' src={post.img} alt='post image' />
         <div className='pt-4 flex gap-3'>
           <LikeButtonComponent />
           {
@@ -38,7 +42,7 @@ export default async function Posts () {
         <div className='pt-4 flex flex-col gap-3'>
           <Comment />
           {
-              post.commentModels.length > 0 ? <Link href='/'> <p> Show comments</p> </Link> : <p>Without comments yet</p>
+              post.commentModels.length > 0 ? <Link href={`post/${post.Post_ID}/comments`}> <p> Show comments</p> </Link> : <p>Without comments yet</p>
             }
         </div>
       </div>
