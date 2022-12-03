cb3dce08851509dec604341e2003a3965bcdeb13
Luis <Luistejeda0011@gmail.com>
2022-12-03 13:15:43 -0400

    Adding react toast and working styles

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,15 @@
+'use client'
+
 import './globals.css'
 import Navbar from '../components/navbar'
+import { Toaster } from 'react-hot-toast'
 export default function MainLayout ({ children }) {
   return (
     <html lang='en'>
       <head />
       <body>
         <Navbar />
+        <Toaster />
         {children}
       </body>
     </html>
