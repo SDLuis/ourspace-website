096b171fc200551b261383dafff4f54048a16fc7
Luis <Luistejeda0011@gmail.com>
2022-12-03 22:58:11 -0400

    Adding middleware conditions

--- middleware.js
+++ middleware.js
@@ -5,10 +5,6 @@ export async function middleware (request) {
   const cookie = request.cookies.get('ourspace')?.value
   const secret = new TextEncoder().encode('ourspace')
 
-  if (cookie) {
-    await jwtVerify(cookie, secret)
-    return NextResponse.next()
-  }
   if (request.nextUrl.pathname.includes('/home')) {
     if (cookie === undefined) {
       return NextResponse.redirect(new URL('/', request.url))
@@ -16,6 +12,17 @@ export async function middleware (request) {
     try {
       await jwtVerify(cookie, secret)
       return NextResponse.next()
+    } catch (err) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
+  if (request.nextUrl.pathname.includes('/login') || request.nextUrl.pathname === '/') {
+    if (cookie === undefined) {
+      return NextResponse.next()
+    }
+    try {
+      await jwtVerify(cookie, secret)
+      return NextResponse.redirect(new URL('/home', request.url))
     } catch (err) {
       return NextResponse.redirect(new URL('/', request.url))
     }
