0606e27b1dfddc8c20bab7d298f39aba8ddc5fb7
Luis <Luistejeda0011@gmail.com>
2022-12-05 11:35:23 -0400

    Removing toaster layout

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,59 +3,53 @@
 import { useEffect } from 'react'
 import Link from 'next/link'
 import UseAuth from '../../fetch/login'
-import { Toaster } from 'react-hot-toast'
 
 export default function LoginComponent () {
-  const { login, setUser, setPassword, disabled, setDisabled, isLogged } = UseAuth()
+  const { login, setUser, setPassword, disabled, isLogged } = UseAuth()
   useEffect(() => {
     if (isLogged) {
       window.location.href = '/home'
     }
   }, [isLogged])
-
   return (
-    <div className='Login grid h-[80vh] sm:h-[75vh] place-items-center'>
-      <Toaster />
-      <main className='w-full h-[80vh] sm:border border-solid border-gray-400 sm:rounded-lg sm:w-[451px] sm:h-[70vh] '>
-        <div className='formulario flex flex-col justify-center items-center gap-4 h-full'>
-          <center>
-            <h2 className='font-medium text-2xl dark:text-gray-200'>Welcome to Ourspace</h2>
-          </center>
-          <div className='w-9/12'>
-            <div className='mb-4'>
-              <label htmlFor='user' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>User</label>
-              <input type='text' id='user' onChange={(e) => { setUser(e.target.value) }} className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='Louis00' required />
+    <form onSubmit={login}>
+      <div className='Login grid h-[80vh] sm:h-[75vh] place-items-center'>
+        <main className='w-full h-[80vh] sm:border border-solid border-gray-400 sm:rounded-lg sm:w-[451px] sm:h-[70vh] '>
+          <div className='formulario flex flex-col justify-center items-center gap-4 h-full'>
+            <center>
+              <h2 className='font-medium text-2xl dark:text-gray-200'>Welcome to Ourspace</h2>
+            </center>
+            <div className='w-9/12'>
+              <div className='mb-4'>
+                <label htmlFor='user' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>User</label>
+                <input type='text' id='user' onChange={(e) => { setUser(e.target.value) }} className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='Louis00' required />
+              </div>
+              <div className='mb-[13px]'>
+                <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Password</label>
+                <input type='password' id='password' onChange={(e) => { setPassword(e.target.value) }} className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='•••••••••' required />
+              </div>
             </div>
-            <div className='mb-[13px]'>
-              <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Password</label>
-              <input type='password' id='password' onChange={(e) => { setPassword(e.target.value) }} className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='•••••••••' required />
+            <div className='w-full flex justify-center'>
+              <button
+                disabled={disabled}
+                className='Button cursor-pointer focus:outline-none disabled:cursor-default disabled:bg-blue-500 bg-blue-800 py-2 text-gray-300 px-10 rounded-r-full rounded-l-full w-9/12'
+                type='submit'
+              >
+                Log In
+              </button>
+
+            </div>
+            <div>
+              <p className='login-card-footer-text dark:text-gray-200 text-lg'>
+                Don't have an account?{' '}
+                <Link href='/register' className='border-b border-b-gray-800 dark:border-b-gray-300 text-gray-800 dark:text-gray-300'>
+                  Register here
+                </Link>
+              </p>
             </div>
           </div>
-          <div className='w-full flex justify-center'>
-            <button
-              disabled={disabled}
-              className='Button focus:outline-none disabled:bg-blue-500 bg-blue-800 py-2 text-gray-300 px-10 rounded-r-full rounded-l-full w-9/12'
-              type='submit'
-              onClick={(e) => {
-                e.preventDefault()
-                setDisabled(true)
-                login()
-                setTimeout(() => { setDisabled(false) }, 2000)
-              }}
-            >
-              Log In
-            </button>
-          </div>
-          <div>
-            <p className='login-card-footer-text dark:text-gray-200 text-lg'>
-              Don't have an account?{' '}
-              <Link href='/register' className='border-b border-b-gray-800 dark:border-b-gray-300 text-gray-800 dark:text-gray-300'>
-                Register here
-              </Link>
-            </p>
-          </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </form>
   )
 }
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -4,10 +4,9 @@
 import { useEffect } from 'react'
 import Link from 'next/link'
 import UseRegister from '../../fetch/register'
-import { Toaster } from 'react-hot-toast'
 
 export default function RegisterComponent () {
-  const { Register, isRegister, form, setForm, disabled, setDisabled } = UseRegister()
+  const { Register, isRegister, form, setForm, disabled } = UseRegister()
 
   useEffect(() => {
     setTimeout(() => {
@@ -18,78 +17,73 @@ export default function RegisterComponent () {
   }, [isRegister])
 
   return (
-    <div className='Register grid h-[95vh] place-items-center'>
-      <Toaster />
-      <main className='w-full h-[100vh] border border-solid border-gray-400 bg-black sm:rounded-lg sm:w-[451px] sm:h-[90vh] '>
-        <div className='formulario w-full flex flex-col gap-4 justify-center items-center h-full'>
-          <center>
-            <h2 className='font-medium text-2xl dark:text-gray-200'>Register</h2>
-          </center>
-          <div className='w-10/12'>
-            <div className='grid gap-6 w-full mb-4 grid-cols-2'>
-              <div>
-                <label htmlFor='first_name' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>First name</label>
+    <form onSubmit={Register}>
+      <div className='Register grid h-[95vh] place-items-center'>
+        <main className='w-full h-[100vh] sm:border border-solid border-gray-400 bg-black sm:rounded-lg sm:w-[451px] sm:h-[90vh] '>
+          <div className='formulario w-full flex flex-col gap-4 justify-center items-center h-full'>
+            <center>
+              <h2 className='font-medium text-2xl dark:text-gray-200'>Register</h2>
+            </center>
+            <div className='w-10/12'>
+              <div className='grid gap-6 w-full mb-4 grid-cols-2'>
+                <div>
+                  <label htmlFor='first_name' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>First name</label>
+                  <input
+                    type='text' id='first_name' onChange={(e) => {
+                      setForm({ ...form, firstNameReg: e.target.value })
+                    }} className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='Louis' required
+                  />
+                </div>
+                <div>
+                  <label htmlFor='last_name' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Last name</label>
+                  <input
+                    type='text' id='last_name' onChange={(e) => {
+                      setForm({ ...form, lastNameReg: e.target.value })
+                    }} className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='Rincon' required
+                  />
+                </div>
+              </div>
+              <div className='mb-4'>
+                <label htmlFor='user' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>User</label>
                 <input
-                  type='text' id='first_name' onChange={(e) => {
-                    setForm({ ...form, firstNameReg: e.target.value })
-                  }} className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='Louis' required
+                  type='text' id='user' onChange={(e) => {
+                    setForm({ ...form, userReg: e.target.value })
+                  }} className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='LRincon' required
                 />
               </div>
-              <div>
-                <label htmlFor='last_name' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Last name</label>
+              <div className='mb-4'>
+                <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Password</label>
                 <input
-                  type='text' id='last_name' onChange={(e) => {
-                    setForm({ ...form, lastNameReg: e.target.value })
-                  }} className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='Rincon' required
+                  type='password' onChange={(e) => {
+                    setForm({ ...form, passwordReg: e.target.value })
+                  }} id='password' className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='•••••••••' required
                 />
               </div>
-            </div>
-            <div className='mb-4'>
-              <label htmlFor='user' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>User</label>
-              <input
-                type='text' id='user' onChange={(e) => {
-                  setForm({ ...form, userReg: e.target.value })
-                }} className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='LRincon' required
-              />
-            </div>
-            <div className='mb-4'>
-              <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>Password</label>
-              <input
-                type='password' onChange={(e) => {
-                  setForm({ ...form, passwordReg: e.target.value })
-                }} id='password' className='border text-sm rounded-lg focus:ring-gray-500 block w-full p-2.5 bg-transparent border-gray-400 placeholder-gray-400 text-white' placeholder='•••••••••' required
-              />
-            </div>
-            <input className='w-[85%] sm:w-full' type='file' onChange={(e) => setForm({ ...form, img: e.target.files[0] })} />
-            {
+              <input className='w-[85%] sm:w-full' type='file' onChange={(e) => setForm({ ...form, img: e.target.files[0] })} />
+              {
               form.img.length !== 0 ? <div className='flex justify-center my-1'> <img className='w-[100px] h-[100px] rounded-full object-cover' src={URL.createObjectURL(form.img)} alt='user img' /> </div> : null
             }
+            </div>
+            <div className='w-full flex justify-center'>
+              <button
+                disabled={disabled}
+                className='Button mb-1 focus:outline-none disabled:bg-blue-500 bg-blue-800 py-2 text-gray-300 px-10 rounded-r-full rounded-l-full w-10/12'
+                type='submit'
+              >
+                Register
+              </button>
+            </div>
+            <div>
+              <p className='login-card-footer-text dark:text-gray-200 text-lg'>
+                Already have an account?{' '}
+                <Link href='/login' className='border-b border-b-gray-300 text-gray-300'>
+                  Log in here
+                </Link>
+              </p>
+            </div>
           </div>
-          <div className='w-full flex justify-center'>
-            <button
-              disabled={disabled}
-              className='Button mb-1 focus:outline-none disabled:bg-blue-500 bg-blue-800 py-2 text-gray-300 px-10 rounded-r-full rounded-l-full w-10/12'
-              type='submit'
-              onClick={(e) => {
-                e.preventDefault()
-                setDisabled(true)
-                Register()
-                setTimeout(() => { setDisabled(false) }, 2000)
-              }}
-            >
-              Register
-            </button>
-          </div>
-          <div>
-            <p className='login-card-footer-text dark:text-gray-200 text-lg'>
-              Already have an account?{' '}
-              <Link href='/login' className='border-b border-b-gray-300 text-gray-300'>
-                Log in here
-              </Link>
-            </p>
-          </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </form>
   )
 }
