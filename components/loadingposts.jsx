fd21be254c35bacdae67ed1d867d20f0c56f70ac
Luis <Luistejeda0011@gmail.com>
2022-12-05 18:41:02 -0400

    Adding post loader component

---
+++ components/loadingposts.jsx
@@ -0,0 +1,19 @@
+import ContentLoader from 'react-content-loader'
+
+const InstagramStyle = props => (
+  <div className='sm:px-8 px-0'>
+    <main className='min-h-screen w-full sm:w-[90.5%] flex justify-center'>
+      <div className='w-full sm:w-[598px] md:w-[470px] lg:w-[598px] py-5 flex flex-col items-center border-x border-solid sm:border-gray-900'>
+        <ContentLoader className='w-[90%] sm:w-[440px] xl:w-[430px]' viewBox='0 0 400 460' {...props}>
+          <circle cx='31' cy='31' r='15' />
+          <rect x='58' y='18' rx='2' ry='2' width='140' height='10' />
+          <rect x='58' y='34' rx='2' ry='2' width='140' height='10' />
+          <rect x='0' y='60' rx='2' ry='2' width='140' height='10' />
+          <rect x='0' y='78' rx='2' ry='2' width='525' height='300' />
+        </ContentLoader>
+      </div>
+    </main>
+  </div>
+)
+
+export default InstagramStyle
