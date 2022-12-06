cde6837f2d6fbd4d31a4cc59abfa05ff496ecc17
Luis <Luistejeda0011@gmail.com>
2022-12-06 13:14:37 -0400

    Adding placeholder by default

--- components/user.jsx
+++ components/user.jsx
@@ -2,12 +2,18 @@
 'use client'
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link'
+import Image from 'next/image'
 import { Back } from './icons'
 import Posts from './posts'
 import { useState } from 'react'
+import placeholder from '../public/placeholder-user.jpg'
+import placeholderCover from '../public/placeholder-portada.webp'
 
 export default function User ({ foundUser, foundPosts }) {
   const [page, setPage] = useState('posts')
+  const img = !foundUser.img ? placeholder : foundUser.img
+  const cover = !foundUser.cover ? placeholderCover : foundUser.cover
+
   return (
     <div className='w-full flex justify-center lg:px-[18%]'>
       <div className='flex flex-col w-full sm:w-[598px] items-center sm:border-x min-h-screen border-solid border-gray-900 pb-6'>
@@ -18,8 +24,8 @@ export default function User ({ foundUser, foundPosts }) {
           </div>
         </div>
         <div className='relative w-full sm:w-[598px] h-[60%] sm:h-[280px]'>
-          <img className='w-full sm:w-[598px] h-full sm:h-[280px] object-cover relative' alt='portada' src='https://img.freepik.com/vector-gratis/conjunto-portadas-arte-abstracto-dibujado-mano_23-2148970324.jpg?w=2000' />
-          <div className='w-32 h-32 sm:w-44 sm:h-44 bg-gray-200 flex justify-center items-center rounded-full absolute -bottom-16 sm:-bottom-20 left-0 right-0 m-auto'><img className='w-28 h-28 sm:w-40 sm:h-40 rounded-full object-cover' alt='userphoto' src={foundUser.img} /></div>
+          <Image width={598} height={280} className='w-full sm:w-[598px] h-full sm:h-[280px] object-cover relative' alt='portada' src={cover} />
+          <div className='w-32 h-32 sm:w-44 sm:h-44 bg-gray-200 flex justify-center items-center rounded-full absolute -bottom-16 sm:-bottom-20 left-0 right-0 m-auto'><Image placeholder={placeholder} width={160} height={160} className='w-28 h-28 sm:w-40 sm:h-40 rounded-full object-cover' alt='userphoto' src={img} /></div>
           <div className='flex justify-between w-[90%] sm:w-[95%] absolute -bottom-[52px] left-0 right-0 m-auto'>
             <button className='sm:w-48 px-5 sm:px-0 py-2 rounded-md bg-slate-800 text-gray-200'>Seguir</button>
             <button className='sm:w-48 px-5 sm:px-0 py-2 rounded-md bg-sky-900 text-gray-200'>Mensaje</button>
