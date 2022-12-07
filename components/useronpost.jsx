6c6a29ab93920eeadff56f4c133ee3cc94c62265
Luis <Luistejeda0011@gmail.com>
2022-12-06 23:25:41 -0400

    Adding object cover attribute to user img on user on post component

--- components/useronpost.jsx
+++ components/useronpost.jsx
@@ -7,7 +7,7 @@ export default function UserOnPost ({ post }) {
   return (
     <div className='flex gap-3 items-center'>
       <Link href={`/user/${post.userModel.user}`}>
-        <Image width={48} height={48} className='rounded-full w-12 h-12 sm:w-14 sm:h-14' src={img} alt='user' />
+        <Image width={48} height={48} className='rounded-full w-12 h-12 sm:w-14 sm:h-14 object-cover' src={img} alt='user' />
       </Link>
       <div className='py-1 h-14 sm:h-16'>
         <Link href={`/user/${post.userModel.user}`}>
