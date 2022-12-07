38305a47aec081f26732649b195b3ec212e1795d
Luis <Luistejeda0011@gmail.com>
2022-12-06 21:12:29 -0400

    Adding sidebar to base route

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import ListOfPosts from '../fetch/listOfPosts'
-import Suggeration from '../components/suggestion'
+import Sidebar from '../components/sidebar'
 import Auth from '../components/auth'
 import AuthBanner from '../components/authbanner'
 
@@ -9,7 +9,7 @@ export default function Home () {
       <div className='sm:px-8 px-0'>
         <main className='min-h-screen w-full lg:w-[90%] flex justify-center md:grid md:grid-cols-[470px_auto] lg:grid-cols-[598px_275px_auto] 1xl:grid-cols-[275px_598px_auto] gap-2 text-white'>
           <div className='hidden 1xl:flex justify-center w-full py-5'>
-            <Suggeration />
+            <Sidebar />
           </div>
           <div className='w-full min-h-screen sm:w-[598px] md:w-[470px] lg:w-[598px] py-5 flex flex-col items-center border-x border-solid sm:border-gray-900'>
             <ListOfPosts />
