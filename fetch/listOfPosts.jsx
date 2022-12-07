5df7f8acfb850260bda45ff12aeec77ba0fea295
Luis <Luistejeda0011@gmail.com>
2022-12-06 22:44:28 -0400

    Adding revalidate fetch

--- fetch/listOfPosts.jsx
+++ fetch/listOfPosts.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-import axios from 'axios'
 import Posts from '../components/posts'
 
 const host = 'https://ourspace-api.up.railway.app'
 
-async function fetchPosts () {
-  return await axios.get(`${host}/posts`, { next: { revalidate: 10 } })
+const fetchPosts = () => {
+  return fetch(`${host}/posts`, { next: { revalidate: 10 } }).then(res => res.json())
 }
 
 export default async function RenderPosts () {
-  const posts = await (await fetchPosts()).data
+  const posts = await (await fetchPosts())
 
   return (
     <Posts posts={posts} />
