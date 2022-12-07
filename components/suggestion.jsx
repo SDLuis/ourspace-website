b7226d56bc357e811b80d8593c204a58d5ce323a
Luis <Luistejeda0011@gmail.com>
2022-12-06 21:13:18 -0400

    Adding correct component name

--- components/suggestion.jsx
+++ components/suggestion.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-export default function Utilities () {
+export default function Suggeration () {
   return (
     <div className='w-[95%] flex h-screen items-center justify-center rounded-md bg-[#000009]'>Suggeration</div>
   )
