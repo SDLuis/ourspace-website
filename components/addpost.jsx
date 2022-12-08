d24a8bff6c86ae6742299f2406bc349312f6dc7e
Luis <Luistejeda0011@gmail.com>
2022-12-07 23:39:18 -0400

    Fixing select same image after remove error

--- components/addpost.jsx
+++ components/addpost.jsx
@@ -4,12 +4,15 @@ import { useEffect, useState, useRef } from 'react'
 import Image from 'next/image'
 import TextareaAutosize from 'react-textarea-autosize'
 import UseUser from '../fetch/login'
+import InputPostType from './inputposttype'
 import { ImageIcon, Delete } from './icons'
 import placeholder from '../public/placeholder-user.jpg'
 
 export default function Addpost () {
   const { userLogged } = UseUser()
   const description = useRef('')
+  const inputImg = useRef('')
+
   const [post, setPost] = useState({
     Post_Type: '',
     Location: '',
@@ -17,6 +20,10 @@ export default function Addpost () {
   })
   const [user, setUser] = useState('')
   const img = !user.img ? placeholder : user.img
+  const removingImg = () => {
+    setPost({ ...post, postImg: [] })
+    inputImg.current.value = ''
+  }
 
   useEffect(() => {
     userLogged().then(data => setUser(data))
@@ -24,22 +31,25 @@ export default function Addpost () {
 
   return (
     <div className='w-full flex justify-center mb-3'>
-      <div className='w-full px-6 flex gap-3'>
+      <div className='w-full px-7 flex gap-3'>
         <Image width={48} height={48} className='rounded-full w-12 h-12 sm:w-14 sm:h-14 object-cover' src={img} alt='user' />
         <div className='w-full'>
+          <div className='flex justify-between'>
+            <InputPostType />
+          </div>
           <TextareaAutosize
             className='w-full min-h-[80px] rounded-md p-2 max-h-[25rem] outline-none border-none leading-6 text-gray-200 text-base sm:text-lg bg-transparent resize-none'
             ref={description} placeholder='Mi@. Fluya!'
           />
           {post.postImg.length !== 0
-            ? <div className='relative'><Image className='w-full h-auto object-cover mb-3 rounded-md' src={URL.createObjectURL(post.postImg)} width={300} height={300} alt='' /> <div className='grid place-items-center absolute top-1 left-1 rounded-full bg-gray-900/80  hover:bg-gray-900/60 transition-all ease-in-out duration-300 p-1'><button onClick={() => { setPost({ ...post, postImg: [] }) }}> <Delete /> </button> </div></div>
+            ? <div className='relative'><Image className='w-full h-auto object-cover mb-3 rounded-md' src={URL.createObjectURL(post.postImg)} width={300} height={300} alt='' /> <div className='grid place-items-center absolute top-1 left-1 rounded-full bg-gray-900/80  hover:bg-gray-900/60 transition-all ease-in-out duration-300 p-1'><button onClick={() => removingImg()}> <Delete /> </button> </div></div>
             : null}
           <div className='flex justify-between items-end'>
             <div>
               <label className='cursor-pointer' htmlFor='file-input'>
                 <ImageIcon />
               </label>
-              <input onChange={(e) => setPost({ ...post, postImg: e.target.files[0] })} className='hidden' id='file-input' type='file' />
+              <input onChange={(e) => setPost({ ...post, postImg: e.target.files[0] })} ref={inputImg} className='hidden' id='file-input' type='file' />
             </div>
             <button className='px-4 py-2 rounded-l-full rounded-r-full bg-sky-600 text-gray-200'>Postear</button>
           </div>
