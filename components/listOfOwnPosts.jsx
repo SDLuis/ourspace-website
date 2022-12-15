1667d690b0a5ad512d15598b15957de0bd63c1b1
Luis <Luistejeda0011@gmail.com>
2022-12-14 22:00:53 -0400

    Adding remove own post option on user profile

--- components/listOfOwnPosts.jsx
+++ components/listOfOwnPosts.jsx
@@ -6,9 +6,13 @@ import Posts from './posts'
 import { UseOwnPosts } from '../hooks/useOwnPosts'
 import LoadingPost from './loadingposts'
 import useNearScreen from '../hooks/useNearScreen'
+import UserLogged from '../hooks/userLogged'
+import UsePosts from '../hooks/usePosts'
 import debounce from 'just-debounce-it'
 
 export default function ListOfOwnPosts ({ prevPosts, user } = {}) {
+  const { userFound } = UserLogged()
+  const { removePost } = UsePosts()
   const { posts, loading, setPage } = UseOwnPosts({ prevPosts, user })
   const externalRef = useRef()
   const { isNearScreen } = useNearScreen({
@@ -27,7 +31,7 @@ export default function ListOfOwnPosts ({ prevPosts, user } = {}) {
   return (
     <div className='w-full'>
       <div className='min-h-screen'>
-        <Posts posts={posts} />
+        <Posts posts={posts} userLogged={userFound} removePost={removePost} />
       </div>
       <div id='visor' ref={externalRef} />
     </div>
