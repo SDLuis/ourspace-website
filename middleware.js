553bdcd5521b86c9681804b66307b70248f8686e
Luis <Luistejeda0011@gmail.com>
2022-12-17 19:40:37 -0400

    Protecting edit user route if user not logged

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ export async function middleware (request) {
   const cookie = request.cookies.get('ourspace')?.value
   const secret = new TextEncoder().encode('ourspace')
 
-  if (request.nextUrl.pathname.includes('/home')) {
+  if (request.nextUrl.pathname.includes('/home') || request.nextUrl.pathname.includes('/user/edit')) {
     if (cookie === undefined) {
       return NextResponse.redirect(new URL('/', request.url))
     }
