88c08ea10e64394096b6ae015b173c1cf619eab5
Luis <Luistejeda0011@gmail.com>
2022-12-18 00:16:27 -0400

    Adding conditional loader on edit user page

--- app/user/edit/page.jsx
+++ app/user/edit/page.jsx
@@ -1,10 +1,12 @@
 'use client'
 import UserLogged from '../../../hooks/userLogged'
 import UserEdit from '../../../components/useredit'
+import Loading from '../../../components/loading'
 
 export default function Page () {
-  const { userFound } = UserLogged()
-  if (!userFound) return
+  const { userFound, loading } = UserLogged()
+
+  if (loading) return <div className='h-screen grid place-items-center'> <Loading /></div>
   return (
     <UserEdit user={userFound} />
   )
