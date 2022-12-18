7df49d5d1569ab7f068772dd3f9c61be28ff7434
Luis <Luistejeda0011@gmail.com>
2022-12-18 00:27:55 -0400

    Working style

--- components/userinfo.jsx
+++ components/userinfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function UserInfo ({ user }) {
   return (
-    <div className='w-full flex justify-center p-4'>
+    <div className='w-full h-[50vh] flex justify-center p-4'>
       <div className='flex flex-col w-[80%] gap-1'>
         <span className='flex justify-between text-lg w-full gap-1'><p className='font-medium text-sky-700'>Ubicación:</p><p>{user.Location ? user.Location : 'Anonimo'} </p></span>
         <span className='flex justify-between text-lg w-full gap-1'><p className='font-medium text-sky-700'>Fecha de nacimiento: </p><p>{user.Date_Of_Birth ? user.Date_Of_Birth : '-- : -- : ----'} </p></span>
