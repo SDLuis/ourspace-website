9b1725e8bc35cf00d3e3de828d886e816012bb62
Luis <Luistejeda0011@gmail.com>
2022-12-17 22:04:04 -0400

    Commenting edit user route

--- app/user/edit/page.jsx
+++ app/user/edit/page.jsx
@@ -1,11 +1,12 @@
 'use client'
 import UserLogged from '../../../hooks/userLogged'
-import UserEdit from '../../../components/useredit'
+// import UserEdit from '../../../components/useredit'
 
 export default function Page () {
   const { userFound } = UserLogged()
   if (!userFound) return
-  return (
+  return null
+  /* return (
     <UserEdit user={userFound} />
-  )
+  ) */
 }
