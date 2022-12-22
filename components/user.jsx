795d4da752e3f67d20cabeb4ec98860d4ff4845b
Luis <Luistejeda0011@gmail.com>
2022-12-22 17:18:53 -0400

    Working styles

--- components/user.jsx
+++ components/user.jsx
@@ -32,7 +32,7 @@ export default function User ({ foundUser, foundPosts }) {
         <div className='relative flex justify-center w-full sm:w-[598px] h-[60%] sm:h-[280px]'>
           <Image width={598} height={280} className='w-full sm:w-[596px] h-full sm:h-[280px] object-cover relative' alt='portada' src={cover} />
           <div hidden={OwnerProfile(foundUser)} className='absolute top-1.5 right-1.5 p-2 rounded-full bg-gray-900/80 hover:bg-gray-900/60 transition-all ease-in-out duration-300'><Link href='/user/editcover'><EditCover /></Link></div>
-          <div className='w-32 h-32 sm:w-44 sm:h-44 bg-gray-200 flex justify-center items-center rounded-full absolute -bottom-16 sm:-bottom-20 left-0 right-0 m-auto'><Image placeholder={placeholder} width={160} height={160} className='w-28 h-28 sm:w-40 sm:h-40 rounded-full object-cover' alt='userphoto' src={img} /></div>
+          <div className='w-32 h-32 sm:w-[10.7rem] sm:h-[10.7rem] bg-black flex justify-center items-center rounded-full absolute -bottom-16 sm:-bottom-20 left-0 right-0 m-auto'><Image placeholder={placeholder} width={160} height={160} className='w-28 h-28 sm:w-40 sm:h-40 rounded-full object-cover' alt='userphoto' src={img} /></div>
           <div className='flex justify-between w-[90%] sm:w-[95%] absolute -bottom-[52px] left-0 right-0 m-auto'>
             <button className='sm:w-48 px-5 sm:px-0 py-2 rounded-md bg-slate-800 text-gray-200'>Seguir</button>
             <button className='sm:w-48 px-5 sm:px-0 py-2 rounded-md bg-sky-900 text-gray-200'>Mensaje</button>
