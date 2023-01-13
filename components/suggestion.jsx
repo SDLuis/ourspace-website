334fec554da4348bb6892b84113c4658da7ed49c
Luis <Luistejeda0011@gmail.com>
2023-01-13 16:43:47 -0400

    Working suggeration component

--- components/suggestion.jsx
+++ components/suggestion.jsx
@@ -1,14 +1,19 @@
 'use client'
-import NewUser from './newusers'
+import NewUser from './newuser'
+import UseUsers from '../hooks/useNewUser'
+import Loading from './loading'
 
-export default function Suggeration ({ users = [] } = {}) {
+export default function Suggeration () {
+  const { othersUsers, loading } = UseUsers()
   return (
     <div className='w-[95%] flex flex-col h-screen items-center justify-start rounded-md'>
       <div className='p-3 bg-[#17181c] w-full rounded-md flex flex-col gap-5'>
         <p className='text-xl font-bold text-[#f2f2f2]'>Nuevos usuarios</p>
-        {users.map((user) => (
-          <NewUser key={user.User_ID} user={user} />
-        ))}
+        {loading
+          ? <div className='w-full h-full flex justify-center items-center'> <Loading /> </div>
+          : othersUsers.map((user) => (
+            <NewUser key={user.User_ID} user={user} />
+          ))}
       </div>
     </div>
   )
