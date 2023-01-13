0c3e476ba6d4c9a92c02053d649e6780a397b877
Luis <Luistejeda0011@gmail.com>
2023-01-12 20:53:12 -0400

    Adding new user server component to suggeration

--- components/suggestion.jsx
+++ components/suggestion.jsx
@@ -1,7 +1,15 @@
 'use client'
+import NewUser from './newusers'
 
-export default function Suggeration () {
+export default function Suggeration ({ users = [] } = {}) {
   return (
-    <div className='w-[95%] flex h-screen items-start justify-center rounded-md'>Suggeration</div>
+    <div className='w-[95%] flex flex-col h-screen items-center justify-start rounded-md'>
+      <div className='p-3 bg-[#17181c] w-full rounded-md flex flex-col gap-5'>
+        <p className='text-xl font-bold text-[#f2f2f2]'>Nuevos usuarios</p>
+        {users.map((user) => (
+          <NewUser key={user.User_ID} user={user} />
+        ))}
+      </div>
+    </div>
   )
 }
