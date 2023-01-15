7c11c564cff71a4f0a3115698b454bf3a9d93cf7
Luis <Luistejeda0011@gmail.com>
2023-01-15 13:37:37 -0400

    Working styles

--- components/suggestion.jsx
+++ components/suggestion.jsx
@@ -7,11 +7,11 @@ export default function Suggeration () {
   const { othersUsers, loading } = UseUsers()
   return (
     <div className='w-[95%] flex flex-col h-screen items-center justify-start rounded-md'>
-      <div className='p-3 bg-[#17181c] w-full rounded-md flex flex-col gap-5'>
+      <div className='p-3 bg-[#17181c] w-full rounded-md flex flex-col gap-4'>
         <p className='text-xl font-bold text-[#f2f2f2]'>Nuevos usuarios</p>
         {loading
-          ? <div className='w-full h-full flex justify-center items-center'> <Loading /> </div>
-          : othersUsers.map((user) => (
+          ? <div className='w-full h-full flex justify-center items-center'><div className='w-20'><Loading /></div> </div>
+          : othersUsers?.map((user) => (
             <NewUser key={user.User_ID} user={user} />
           ))}
       </div>
