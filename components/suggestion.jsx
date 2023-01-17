50c00f6ce89644f2583315b07817bbffc2a673de
Luis <Luistejeda0011@gmail.com>
2023-01-16 20:47:15 -0400

    Working styles

--- components/suggestion.jsx
+++ components/suggestion.jsx
@@ -7,7 +7,7 @@ export default function Suggeration () {
   const { othersUsers, loading } = UseUsers()
   return (
     <div className='w-[95%] flex flex-col h-screen items-center justify-start rounded-md'>
-      <div className='p-3 bg-[#17181c] w-full rounded-md flex flex-col gap-4'>
+      <div className='p-3 xl:px-4 xl:py-4 bg-[#17181c] w-full rounded-md flex flex-col gap-4'>
         <p className='text-xl font-bold text-[#f2f2f2]'>Nuevos usuarios</p>
         {loading
           ? <div className='w-full h-full flex justify-center items-center'><div className='w-20'><Loading /></div> </div>
