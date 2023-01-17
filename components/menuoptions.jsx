e682bc295d9a11ac5acf1709b1ac450ec06a04af
Luis <Luistejeda0011@gmail.com>
2023-01-17 16:54:47 -0400

    Adding confirmation on delete post

--- components/menuoptions.jsx
+++ components/menuoptions.jsx
@@ -9,8 +9,8 @@ export default function MenuOptions ({ removePost, post, OwnerPost } = {}) {
       <div id='dropdownInformation' className={` ${show ? 'absolute z-10 top-10 right-2' : 'hidden'} w-44 rounded divide-y shadow bg-gray-700 divide-gray-600`}>
         <div className='p-2 text-sm text-gray-200'>
           <ul className='py-1 text-sm text-gray-200' aria-labelledby='dropdownInformationButton'>
-            <li className='py-2 px-4 hover:bg-gray-600 w-full' onClick={() => setShow(!show)}>
-              <button onClick={() => removePost(post.Post_ID)} className='hover:text-white'>Eliminar</button>
+            <li className='py-2 px-4 hover:bg-gray-600 w-full cursor-pointer' onClick={() => setShow(!show)}>
+              <button onClick={() => removePost(post.Post_ID)} className='hover:text-white w-full'>Eliminar</button>
             </li>
           </ul>
         </div>
--- hooks/usePosts.jsx
+++ hooks/usePosts.jsx
@@ -3,6 +3,8 @@
 
 import { useState, useEffect } from 'react'
 import { toast } from 'react-hot-toast'
+import { confirmAlert } from 'react-confirm-alert'
+import '../styles/reactConfirmAlert.css'
 import PostService, { deletePost } from '../services/posts'
 
 export default function UsePosts ({ prevPosts = [] } = {}) {
@@ -29,10 +31,35 @@ export default function UsePosts ({ prevPosts = [] } = {}) {
   }, [page])
 
   const removePost = (PostID) => {
-    deletePost(PostID).then(({ data }) => {
-      toast.success(data)
-      setTimeout(() => { window.location.reload() }, 700)
-    }).catch(err => toast.error(err))
+    const options = {
+      title: 'Eliminar publicación',
+      message: '¿Seguro que quiere eliminar esta publicación?',
+      buttons: [
+        {
+          label: 'Eliminar',
+          onClick: () => {
+            deletePost(PostID).then(({ data }) => {
+              toast.success(data)
+              setTimeout(() => { window.location.reload() }, 700)
+            }).catch(err => toast.error(err))
+          }
+        },
+        {
+          label: 'No',
+          onClick: () => {}
+        }
+      ],
+      closeOnEscape: true,
+      closeOnClickOutside: true,
+      keyCodeForClose: [8, 32],
+      willUnmount: () => {},
+      afterClose: () => {},
+      onClickOutside: () => {},
+      onKeypress: () => {},
+      onKeypressEscape: () => {},
+      overlayClassName: 'overlay-custom-class-name'
+    }
+    confirmAlert(options)
   }
 
   return { posts, setPosts, loading, setPage, removePost }
