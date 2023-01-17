f4bb4aeb155c928c91f782a1fda9e413f5ae0f5e
Luis <Luistejeda0011@gmail.com>
2023-01-17 15:48:29 -0400

    Edit post removed

--- components/menuoptions.jsx
+++ components/menuoptions.jsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import { MoreOptions } from './icons'
 import UseMenuOption from '../hooks/useMenuOption'
 
@@ -10,9 +9,6 @@ export default function MenuOptions ({ removePost, post, OwnerPost } = {}) {
       <div id='dropdownInformation' className={` ${show ? 'absolute z-10 top-10 right-2' : 'hidden'} w-44 rounded divide-y shadow bg-gray-700 divide-gray-600`}>
         <div className='p-2 text-sm text-gray-200'>
           <ul className='py-1 text-sm text-gray-200' aria-labelledby='dropdownInformationButton'>
-            <li onClick={() => setShow(!show)}>
-              <Link href='/post/edit' className='block py-2 px-4  hover:bg-gray-600 hover:text-white'>Editar</Link>
-            </li>
             <li className='py-2 px-4 hover:bg-gray-600 w-full' onClick={() => setShow(!show)}>
               <button onClick={() => removePost(post.Post_ID)} className='hover:text-white'>Eliminar</button>
             </li>
