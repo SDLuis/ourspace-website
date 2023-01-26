cf1a9ab54745516a94dbfaf2295d7f4de07da475
Luis <Luistejeda0011@gmail.com>
2023-01-25 22:15:26 -0400

    Send message working, without socket yet

--- components/message.jsx
+++ components/message.jsx
@@ -9,22 +9,24 @@ import TextareaAutosize from 'react-textarea-autosize'
 import useMessage from '../hooks/useMessage'
 
 export default function Message ({ ConversationID = null }) {
-  const { loading, messages, userFound } = useMessage({ ConversationID })
+  const { loading, messages, userFound, description, sendMessage, scrollRef } = useMessage({ ConversationID })
 
   if (ConversationID && loading) return <div className='w-full h-full rounded-md bg-black grid place-items-center'><div className='w-10 h-10'><Loading /></div></div>
   return (
-    <div className='w-full h-full relative rounded-md bg-black py-6 px-20'>
-      {
+    <div className='w-full h-full relative rounded-md bg-black'>
+      <div className='h-[calc(100%-92px)] overflow-y-scroll scroll py-6 px-20'>
+        {
         messages?.map((message) => (
-          <div className={`w-full flex gap-1.5 ${message?.userModel.User_ID === userFound?.User_ID ? '' : 'flex-row-reverse'}`} key={message.Message_ID}>
+          <div ref={scrollRef} className={`w-full flex gap-1.5 mb-2 ${message?.userModel.User_ID === userFound?.User_ID ? '' : 'flex-row-reverse'}`} key={message.Message_ID}>
             <Image className='w-10 h-10 rounded-full object-cover' src={message.userModel.img ?? placeholder} width={64} height={64} alt='user' />
             <p className={`h-10 rounded-md p-4 flex items-center font-medium text-[#f2f2f2] ${message?.userModel.User_ID === userFound?.User_ID ? 'bg-sky-700' : 'bg-gray-700'}`}>{message.description}</p>
           </div>
         ))
       }
+      </div>
       <div className={` ${!ConversationID ? 'hidden' : ''} absolute flex gap-2 bottom-2 w-full left-0 right-0 px-2`}>
-        <TextareaAutosize className='w-full min-h-[90px] max-h-[90px] rounded-r-md outline-none rounded-l-md px-2 py-1 font-medium resize-none' />
-        <button><Send /></button>
+        <TextareaAutosize ref={description} className='w-full min-h-[90px] max-h-[90px] rounded-r-md outline-none rounded-l-md px-2 py-1 font-medium resize-none' />
+        <button onClick={() => sendMessage()}><Send /></button>
       </div>
     </div>
   )
--- hooks/useMessage.jsx
+++ hooks/useMessage.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-expressions */
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import UserLogged from './userLogged'
+import { toast } from 'react-hot-toast'
 
 export default function useMessage ({ ConversationID = null } = {}) {
   const { userFound } = UserLogged()
+  const scrollRef = useRef()
   const [messages, setMessages] = useState([])
+  const description = useRef(null)
   const [loading, setLoading] = useState(true)
   const [conversations, setConversations] = useState([])
 
@@ -18,5 +21,18 @@ export default function useMessage ({ ConversationID = null } = {}) {
     setLoading(true)
     ConversationID ? axios.get(`https://ourspace-api-hw4y.onrender.com/messages/find/${ConversationID}`, { withCredentials: true }).then(({ data }) => { setMessages(data) }).finally(setTimeout(() => { setLoading(false) }, 600)) : null
   }, [ConversationID])
-  return { conversations, messages, loading, userFound }
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [messages])
+
+  function sendMessage () {
+    const body = {
+      Conversation_ID: ConversationID,
+      Sender_ID: UserLogged?.User_ID,
+      description: description?.current?.value
+    }
+    description?.current?.value ? axios.post('https://ourspace-api-hw4y.onrender.com/messages/add', body, { withCredentials: true }).then(() => { description.current.value = '' }) : toast.error('Escriba un mensaje')
+  }
+  return { conversations, messages, loading, userFound, sendMessage, description, scrollRef }
 }
