0359679295514547b4670979eff505059edb1f32
Luis <Luistejeda0011@gmail.com>
2023-01-26 23:28:13 -0400

    Changing country api url

--- hooks/useAddpost.jsx
+++ hooks/useAddpost.jsx
@@ -20,7 +20,7 @@ export default function UseAddPost () {
   useEffect(() => {
     userLogged().then(data => setUser(data))
     country && country.trim() !== 'anonimo'
-      ? axios.get(`https://country-api.up.railway.app/country/${country.trim()}`).then(({ data }) => { setCities(data) })
+      ? axios.get(`https://country-api-cq2y.onrender.com/country/${country.trim()}`).then(({ data }) => { setCities(data) })
       : null
   }, [country, disabled])
 
