c0e6f44cea3db6cf32dc29e83b29fb5fc76d16a3
Luis <Luistejeda0011@gmail.com>
2023-01-26 23:12:51 -0400

    Adding dinamic user value on server request

--- app/user/[user]/page.jsx
+++ app/user/[user]/page.jsx
@@ -5,14 +5,14 @@ export default async function UserPage ({ params }) {
   const host = 'https://ourspace-api-hw4y.onrender.com'
 
   async function findUser () {
-    return await fetch(`${host}/users/find/maria`).then(res => res.json())
+    return await fetch(`${host}/users/find/${user}`).then(res => res.json())
   }
   async function findPosts () {
-    return await fetch(`${host}/posts/user/maria`).then(res => res.json())
+    return await fetch(`${host}/posts/user/${user}`).then(res => res.json())
   }
 
   const foundUser = await (await findUser())
-  const foundPosts = await (await findPosts(user))
+  const foundPosts = await (await findPosts())
 
   return (
     <User foundPosts={foundPosts} foundUser={foundUser} />
