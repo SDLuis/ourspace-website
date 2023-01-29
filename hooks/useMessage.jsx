f58e743ff017544934fb0543f4616dbe21cd5b50
Luis <Luistejeda0011@gmail.com>
2023-01-29 15:26:10 -0400

    Adding socket to send message hook

--- hooks/useMessage.jsx
+++ hooks/useMessage.jsx
@@ -2,15 +2,18 @@
 import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import UserLogged from './userLogged'
-import { toast } from 'react-hot-toast'
+import { io } from 'socket.io-client'
 
-export default function useMessage ({ ConversationID = null } = {}) {
+export default function useMessage ({ currentConversation = null } = {}) {
+  const PORT = process.env.PORT || 5000
   const { userFound } = UserLogged()
   const scrollRef = useRef()
   const [messages, setMessages] = useState([])
   const description = useRef(null)
   const [loading, setLoading] = useState(true)
+  const [arrivalMessage, setArrivalMessage] = useState(null)
   const [conversations, setConversations] = useState([])
+  const socket = useRef()
 
   useEffect(() => {
     userFound ? axios.get('https://ourspace-api-hw4y.onrender.com/conversations/owner', { withCredentials: true }).then(({ data }) => setConversations(data)) : null
@@ -19,20 +22,63 @@ export default function useMessage ({ ConversationID = null } = {}) {
   useEffect(() => {
     setMessages([])
     setLoading(true)
-    ConversationID ? axios.get(`https://ourspace-api-hw4y.onrender.com/messages/find/${ConversationID}`, { withCredentials: true }).then(({ data }) => { setMessages(data) }).finally(setTimeout(() => { setLoading(false) }, 600)) : null
-  }, [ConversationID])
+    currentConversation?.Conversation_ID ? axios.get(`https://ourspace-api-hw4y.onrender.com/messages/find/${currentConversation?.Conversation_ID}`, { withCredentials: true }).then(({ data }) => { setMessages(data) }).finally(setTimeout(() => { setLoading(false) }, 600)) : null
+  }, [currentConversation?.Conversation_ID])
 
   useEffect(() => {
     scrollRef.current?.scrollIntoView({ behavior: 'smooth' })
   }, [messages])
 
-  function sendMessage () {
-    const body = {
-      Conversation_ID: ConversationID,
+  useEffect(() => {
+    socket.current = io(`ws://localhost:${PORT}`)
+    userFound ? socket.current.emit('addUser', userFound?.User_ID) : null
+    socket.current.on('getMessage', (data) => {
+      setArrivalMessage({
+        Sender_ID: data.Sender_ID,
+        description: data.description,
+        userModel: { img: data?.img },
+        createdAt: Date.now()
+      })
+    })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userFound])
+
+  useEffect(() => {
+    arrivalMessage &&
+      currentConversation?.members.includes(arrivalMessage.Sender_ID) &&
+      setMessages((prev) => [...prev, arrivalMessage])
+  }, [arrivalMessage, currentConversation])
+
+  const sendMessage = async () => {
+    const message = {
+      Conversation_ID: currentConversation?.Conversation_ID,
       Sender_ID: UserLogged?.User_ID,
       description: description?.current?.value
     }
-    description?.current?.value ? axios.post('https://ourspace-api-hw4y.onrender.com/messages/add', body, { withCredentials: true }).then(() => { description.current.value = '' }) : toast.error('Escriba un mensaje')
+
+    try {
+      const res = await axios.post('https://ourspace-api-hw4y.onrender.com/messages/add', message, { withCredentials: true })
+      let messageWithUserInfo = {}
+      messageWithUserInfo = res.data
+      messageWithUserInfo.userModel = { img: userFound?.img }
+      setMessages([...messages, messageWithUserInfo])
+    } catch (err) {
+      console.log(err)
+    }
+
+    const receiverId = currentConversation?.members.find(
+      (member) => member !== userFound.User_ID
+    )
+
+    receiverId
+      ? socket.current.emit('sendMessage', {
+        Sender_ID: userFound.User_ID,
+        receiverId,
+        img: userFound?.img,
+        description: description?.current?.value
+      })
+      : null
+    description.current.value = ''
   }
   return { conversations, messages, loading, userFound, sendMessage, description, scrollRef }
 }
